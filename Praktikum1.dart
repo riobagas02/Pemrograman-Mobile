bf43c5e1c7caa92a35cd929ec563347f7d1b58c3
Rio Bagas <riobagas02@gmail.com>
2024-09-20 08:47:45 +0700

    Praktikum1

---
+++ Praktikum1.dart
@@ -0,0 +1,44 @@
+// Langkah 1
+
+// void main() {
+//   String test = "test2";
+//   if (test == "test1") {
+//     print("Test1");
+//   } else if (test == "test2") {
+//     print("Test2");
+//   } else {
+//     print("Something else");
+//   }
+
+//   if (test == "test2") print("Test2 again");
+// }
+
+// Langkah 2
+
+// Hasil dari run adalah
+
+// Test2
+// Test2 again
+
+// Langkah 3
+
+void main() {
+  // String test = "test2";
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
+
+  String test = "true";
+  if (test == "true") {
+    print("Kebenaran");
+  }
+
+  if (test == "test2") print("Test2 again");
+}
+
+// Hasil awalnya adalah error, namun setelah diperbaiki maka hasilnya menjadi
+// "Kebenaran"
