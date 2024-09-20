ace09e4b0a72bd55f13ed6d140d48f6eaa1e1276
Rio Bagas <riobagas02@gmail.com>
2024-09-20 14:09:41 +0700

    Praktikum2

---
+++ Praktikum2.dart
@@ -0,0 +1,42 @@
+// Langkah 1
+
+// void main(){
+//   while (counter < 33) {
+//   print(counter);
+//   counter++;
+//   }
+// }
+
+// Hasil Error
+
+// Langkah 1
+// void main() {
+//   int counter = 1;
+//   while (counter < 33) {
+//     print(counter);
+//     counter++;
+//   }
+// }
+
+// ---------------------------------------------------------------
+
+// Langkah3
+
+// void main() {
+//   do {
+//   print(counter);
+//   counter++;
+// } while (counter < 77);
+// }
+
+// Langkah3
+
+  void main() {
+    int counter = 1;
+    do {
+      print(counter);
+      counter++;
+    } while (counter < 77);
+  }
+
+// ---------------------------------------------------------------
