81f9640b6bad564608d54700585fca231c43f098
Rio Bagas <riobagas02@gmail.com>
2024-11-22 09:37:06 +0700

    pertemuan9

---
+++ kamera_flutter/lib/widget/displaypicture_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display the Picture - 2241720193'),
+      ),
+      body: Image.file(
+        File(imagePath),
+      ),
+    );
+  }
+}
